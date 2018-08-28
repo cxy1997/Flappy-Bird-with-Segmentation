718d1c43167b87eb0603ce5f64cb3273d8cc7f41
cxy1997 <1023764859@qq.com>
2018-08-28 02:10:37 -0700

    Finished

--- env.py
+++ env.py
@@ -6,11 +6,11 @@ import cv2
 from itertools import cycle
 from gym.spaces import Box, Discrete
 
-SCREENWIDTH  = 288
+SCREENWIDTH = 288
 SCREENHEIGHT = 512
 
-PIPEGAPSIZE = 100 # gap between upper and lower part of pipe
-BASEY = SCREENHEIGHT * 0.79
+PIPEGAPSIZE = 100  # gap between upper and lower part of pipe
+BASEY = int(SCREENHEIGHT * 0.79)
 
 PLAYER_WIDTH = 34
 PLAYER_HEIGHT = 24
@@ -19,6 +19,7 @@ PIPE_WIDTH = 52
 PIPE_HEIGHT = 320
 
 PLAYER_INDEX_GEN = cycle([0, 1, 2, 1])
+
 
 class FlappyBirdEnv(object):
     def __init__(self):
@@ -36,8 +37,9 @@ class FlappyBirdEnv(object):
             cv2.imread(os.path.join('assets', 'sprites', 'redbird-downflap.png'), -1)
         )
         self.background = cv2.imread(os.path.join('assets', 'sprites', 'background-black.png'))
-        self.pipe = cv2.imread(os.path.join('assets', 'sprites', 'pipe-green.png'), -1)
-        self.base = cv2.imread(os.path.join('assets', 'sprites', 'base.png'))
+        self.lowerpipe = cv2.imread(os.path.join('assets', 'sprites', 'pipe-green.png'), -1)
+        self.upperpipe = np.rot90(self.lowerpipe, k=2)
+        self.base = cv2.imread(os.path.join('assets', 'sprites', 'base.png'), -1)
 
         # Dynamics config
         self.pipeVelX = -4       # player's velocity along Y
@@ -58,15 +60,17 @@ class FlappyBirdEnv(object):
         self.basex = 0
 
         new_pipes = [
-            get_random_pipe(pipeX=SCREENWIDTH),
-            get_random_pipe(pipeX=SCREENWIDTH + (SCREENWIDTH / 2))
+            get_random_pipe(pipeX=SCREENWIDTH - PIPE_WIDTH - 3),
+            get_random_pipe(pipeX=SCREENWIDTH - PIPE_WIDTH - 3 + int(SCREENWIDTH / 2))
         ]
         self.upper_pipes = list(map(lambda x: x['upper_pipe'], new_pipes))
         self.lower_pipes = list(map(lambda x: x['lower_pipe'], new_pipes))
 
         self.playerVelY = 0         # player's velocity along Y, default same as playerFlapped
 
-        return self.render(), self.get_info()
+        obs, self.segmentation, self.collision = self.render()
+        self.collision = self.collision or self.playery >= BASEY - PLAYER_HEIGHT
+        return obs, self.get_info()
 
     def step(self, action):
         self.timestep += 1
@@ -106,39 +110,35 @@ class FlappyBirdEnv(object):
             lPipe['x'] += self.pipeVelX
 
         # add new pipe when first pipe is about to touch left of screen
-        if 0 < self.upper_pipes[0]['x'] < 5:
+        if 0 < self.upper_pipes[0]['x'] < -self.pipeVelX:
             new_pipe = get_random_pipe()
             self.upper_pipes.append(new_pipe['upper_pipe'])
             self.lower_pipes.append(new_pipe['lower_pipe'])
 
         # remove first pipe if its out of the screen
-        if self.upper_pipes[0]['x'] < -PIPE_WIDTH:
+        if self.upper_pipes[0]['x'] <= -PIPE_WIDTH:
             self.upper_pipes.pop(0)
             self.lower_pipes.pop(0)
 
-        # check if crash here
-        isCrash= checkCrash({'x': self.playerx, 'y': self.playery,
-                             'index': self.playerIndex},
-                            self.upperPipes, self.lowerPipes)
-        if check_crash(
-            playerx=self.playerx,
-            playery=self.playery,
-            upper_pipes=self.upper_pipes,
-            lower_pipes=self.lower_pipes,
-            player_hitbox=self.player[self.playerIndex][:, :, 3] > 0,
-            pipe_hitbox=self.pipe[:, :, ] > 0
-        ):
+        obs, self.segmentation, self.collision = self.render()
+        self.collision = self.collision or self.playery >= BASEY - PLAYER_HEIGHT
+        if self.collision:
             terminal = True
-            self.__init__()
             reward = -1
+        return obs, reward, terminal, self.get_info()
 
     def render(self):
-        return None
+        obs = np.array(self.background, dtype=np.uint8)
+        seg = np.zeros(self.background.shape[:-1], dtype=np.uint8)
+        obs, seg, _ = draw_upon(obs, seg, self.base, self.basex, BASEY, 1)
+        for upperpipe, lowerpipe in zip(self.upper_pipes, self.lower_pipes):
+            if -PIPE_WIDTH < upperpipe['x'] < SCREENWIDTH:
+                obs, seg, _ = draw_upon(obs, seg, self.upperpipe, upperpipe['x'], upperpipe['y'], 2)
+                obs, seg, _ = draw_upon(obs, seg, self.lowerpipe, lowerpipe['x'], lowerpipe['y'], 2)
+        obs, seg, collision = draw_upon(obs, seg, self.player[self.playerIndex], self.playerx, self.playery, 3)
+        return obs, seg, collision
 
-    def get_segmentation(self):
-        return None
-
-    def self.get_info(self):
+    def get_info(self):
         info = dict()
         info['playerx'] = self.playerx
         info['playery'] = self.playery
@@ -146,11 +146,30 @@ class FlappyBirdEnv(object):
         info['lower_pipes'] = self.lower_pipes
         info['playerVelX'] = -self.pipeVelX
         info['playerVelY'] = self.playerVelY
-        info['segmentation'] = self.get_segmentation()
+        info['segmentation'] = self.segmentation
+        info['collision'] = self.collision
         return info
 
-def check_crash(playerx, playery, upper_pipes, lower_pipes, player_hitbox, pipe_hitbox):
-    return True
+
+def draw_upon(obs, seg, obj, x, y, classID):
+    x, y = y, x
+    H, W, C = obs.shape
+    assert C == 3
+    h, w, c = obj.shape
+    assert c == 3 or c == 4
+    if c == 3:
+        obj = np.concatenate([obj, 255 * np.ones((h, w), dtype=np.uint8)], axis=2)
+    obj_x_start, obj_x_end = max(-x, 0), min(h, H-x)
+    obj_y_start, obj_y_end = max(-y, 0), min(w, W-y)
+
+    patch = obj[obj_x_start: obj_x_end, obj_y_start: obj_y_end, :3]
+    seg_mask = obj[obj_x_start: obj_x_end, obj_y_start: obj_y_end, 3] > 0
+    collision = np.any(seg_mask * seg[x+obj_x_start: x+obj_x_end, y+obj_y_start: y+obj_y_end] > 0) if classID == 3 else None
+    rgb_mask = np.expand_dims(seg_mask, axis=2)
+    obs[x+obj_x_start: x+obj_x_end, y+obj_y_start: y+obj_y_end, :3] = patch * rgb_mask + obs[x+obj_x_start: x+obj_x_end, y+obj_y_start: y+obj_y_end, :3] * (1-rgb_mask)
+    seg[x+obj_x_start: x+obj_x_end, y+obj_y_start: y+obj_y_end] = classID * seg_mask + seg[x+obj_x_start: x+obj_x_end, y+obj_y_start: y+obj_y_end] * (1-seg_mask)
+    return obs, seg, collision
+
 
 def get_random_pipe(pipeX=None):
     gapY = np.random.choice([20, 30, 40, 50, 60, 70, 80, 90])
@@ -164,5 +183,13 @@ def get_random_pipe(pipeX=None):
         'lower_pipe': {'x': pipeX, 'y': gapY + PIPEGAPSIZE}
     }
 
+
 if __name__ == '__main__':
     env = FlappyBirdEnv()
+    obs, info = env.reset()
+    print(info)
+    for i in range(40):
+        obs, reward, terminal, info = env.step(int(input()))
+        print(terminal)
+        if terminal:
+            obs, info = env.reset()
